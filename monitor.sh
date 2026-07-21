38e25c7ca4b7d6d53ebcfacb9cb627714952d3c6
Apple <apple@Austins-MacBook-Pro.local>
2026-07-21 14:10:55 +0530

    Initial project setup and header

---
+++ monitor.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "========================================"
+echo "   Linux System Health Monitor"
+echo "========================================"
+echo ""
+
+echo "Hostname:"
+hostname
+
+echo ""
+
+echo "Current Date & Time:"
+date
