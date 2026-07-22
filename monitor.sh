8fecb7ddf76eb49cd97872de6424b72c48776266
Austin Thomas <austinthomasfernando@gmail.com>
2026-07-22 15:30:39 +0530

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-echo "========================================"
-echo "   Linux System Health Monitor"
-echo "========================================"
-echo ""
+# ===============================
+# Linux Server Health Monitor
+# Version 1.0
+# Author: Austin Thomas Fernando
+# ===============================
 
-echo "Hostname:"
-hostname
+# Colors
+GREEN="\033[0;32m"
+BLUE="\033[1;34m"
+YELLOW="\033[1;33m"
+RED="\033[0;31m"
+NC="\033[0m"
 
-echo ""
+clear
 
-echo "Current Date & Time:"
-date
+echo -e "${BLUE}"
+echo "=============================================="
+echo "       Linux Server Health Monitor"
+echo "=============================================="
+echo -e "${NC}"
